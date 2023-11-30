1c1b3596aa93b4329c2e177b2f5404d2d247fe3b
Tamir <tamirdude96@gmail.com>
2023-11-30 23:45:40 +0200

    changing ui

--- src/app/components/ButtonGroupSelector/ButtonGroupSelector.tsx
+++ src/app/components/ButtonGroupSelector/ButtonGroupSelector.tsx
@@ -28,13 +28,13 @@ export const ButtonGroupSelector = ({ elementProps }: ButtonGroupProps) => {
             key={elementProp.title}
             value={elementProp.title}
             className={({ checked }) =>
-              `${checked ? 'bg-secondary/75 text-white' : 'bg-white'}
-                    cursor-pointer rounded-md shadow-lg focus:outline-none w-1/2 text-sm flex flex-row justify-evenly h-1/2`
+              `${checked ? 'bg-[#EE09CE] text-white' : 'bg-gray-400'}
+                    cursor-pointer rounded-md shadow-lg focus:outline-none w-1/2 text-sm flex flex-row justify-evenly items-center h-1/2`
             }
           >
-            <div>{elementProp.prefixIcon}</div>
-            <div className=''>{elementProp.title}</div>
-            <div>{elementProp.suffixIcon}</div>
+            {elementProp.prefixIcon}
+            {elementProp.title}
+            {elementProp.suffixIcon}
           </RadioGroup.Option>
 
       ))}
