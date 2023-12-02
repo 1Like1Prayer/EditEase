efd4e7670ecd0ddd7349cb275e4a6b1e8bbbc642
idanpeduiem <62710926+idanpeduiem@users.noreply.github.com>
2023-12-02 20:58:31 +0200

    add icons

--- src/app/components/EditSubscriptionDialog/EditSubscriptionDialog.tsx
+++ src/app/components/EditSubscriptionDialog/EditSubscriptionDialog.tsx
@@ -1,45 +1,85 @@
-'use client'
-import {Select, SelectOptionType} from "@/app/components/Select/Select";
+'use client';
+import { Select, SelectOptionType } from '@/app/components/Select/Select';
+import {
+  AlignCenterVertical,
+  AlignEndVertical,
+  AlignRight,
+  AlignStartVertical,
+  Bold,
+  Italic,
+} from 'lucide-react';
 
-const FONT_OPTIONS: SelectOptionType[] = [{title: 'Futura'}, {title: 'Ariel'}, {title: "David"}]
+const FONT_OPTIONS: SelectOptionType[] = [
+  { title: 'Futura' },
+  { title: 'Ariel' },
+  { title: 'David' },
+];
 
 interface EditSubscriptionDialogProps {
-    isOpen: boolean
+  isOpen: boolean;
 }
 
-export const EditSubscriptionDialog = ({isOpen}: EditSubscriptionDialogProps) => {
-
-    return <div className='card text-sm'>
-        <div className='grid grid-cols-2 gap-2'>
-            <div className='card bg-white'>
-                <div>Background Colour</div>
-                <div></div>
-            </div>
-            <div className='card bg-white'>
-                <div>Text Colour</div>
-                <div></div>
-            </div>
-            <div className='card bg-white col-span-2'>
-                Time being Said / shown: 00:43 - 01:02
-            </div>
-            <div className='card bg-white col-span-2 flex flex-row justify-between items-center'>
-                <div>Font</div>
-                <div className='flex gap-2'>
-                    <Select options={FONT_OPTIONS}/>
-                    <button className={"button"}>B</button>
-                    <button className={"button"}><i>I</i></button>
-                </div>
-            </div>
-            <div className='card bg-white col-span-2'>
-                Alignment
-            </div>
-            <div className='card bg-white col-span-2'>
-                Add Gif to Word
-            </div>
-            <div className='card bg-white col-span-2'>
-                Add Sound to Word
-            </div>
+export const EditSubscriptionDialog = ({
+  isOpen,
+}: EditSubscriptionDialogProps) => {
+  return (
+    <div className='card bg-gray-400 text-sm'>
+      <div className='grid grid-cols-2 gap-2'>
+        <div className='card'>
+          <div>Background Colour</div>
+          <div></div>
         </div>
+        <div className='card'>
+          <div>Text Colour</div>
+          <div></div>
+        </div>
+        <div className='card col-span-2 flex flex-row items-center justify-between'>
+          <div>Time being Said / shown</div>
+          <div>00:43 - 01:02</div>
+        </div>
+        <div className='card col-span-2 flex flex-row items-center justify-between'>
+          <div>Font</div>
+          <div className='flex gap-2'>
+            <Select options={FONT_OPTIONS} />
+            <button className={'button'}>
+              <Bold />
+            </button>
+            <button className={'button bg-transparent shadow-none'}>
+              <Italic />
+            </button>
+          </div>
+        </div>
+        <div className='card col-span-2 flex flex-row items-center justify-between'>
+          <div>Alignment</div>
+          <div className='flex gap-2'>
+            <button className={'button bg-transparent shadow-none'}>
+              <AlignStartVertical />
+            </button>
+            <button className={'button bg-white shadow-none'}>
+              <AlignCenterVertical />
+            </button>
+            <button className={'button bg-transparent shadow-none'}>
+              <AlignEndVertical />
+            </button>
+          </div>
+        </div>
+        <div className='card col-span-2 flex flex-row items-center justify-between'>
+          <div>Add Gif to Word</div>
+          <div className='flex items-center gap-2'>
+            <div>upload GIF</div>
+            <button className={'button bg-transparent shadow-none'}>
+              <AlignStartVertical />
+            </button>
+            <button className={'button bg-transparent shadow-none'}>
+              <AlignEndVertical />
+            </button>
+          </div>
+        </div>
+        <div className='card col-span-2 flex flex-row items-center justify-between'>
+          <div>Add Sound to Word</div>
+          <div>upload Sound</div>
+        </div>
+      </div>
     </div>
-
-}
+  );
+};
